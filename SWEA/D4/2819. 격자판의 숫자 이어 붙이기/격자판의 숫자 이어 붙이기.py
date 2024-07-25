04cad3158db298dbb00e49017980459226cd1019
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-07-25 14:55:05 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 593 ms, Memory: 81,588 KB -BaekjoonHub

---
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -0,0 +1,35 @@
+def search_dfs(grid, i, j, visited, visited_set, depth=0):
+    if depth == 7:
+        visited_set.add(tuple(visited))
+        return
+
+    visited.append(grid[i][j])
+
+    if i < len(grid) - 1:
+        search_dfs(grid, i + 1, j, visited, visited_set, depth + 1)  # 아래로 이동
+    if i > 0:
+        search_dfs(grid, i - 1, j, visited, visited_set, depth + 1)  # 위로 이동
+    if j < len(grid[0]) - 1:
+        search_dfs(grid, i, j + 1, visited, visited_set, depth + 1)  # 오른쪽 이동
+    if j > 0:
+        search_dfs(grid, i, j - 1, visited, visited_set, depth + 1)  # 왼쪽 이동
+
+    visited.pop()
+
+
+def main():
+    T = int(input())
+    N = 4
+
+    for test_case in range(1, T + 1):
+        grid = [input().split() for _ in range(N)]
+
+        visited_set = set()
+        for i in range(N):
+            for j in range(N):
+                search_dfs(grid, i, j, [], visited_set)
+        print(f"#{test_case} {len(visited_set)}")
+
+
+if __name__ == "__main__":
+    main()
