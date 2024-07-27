76e10d3a3a9df6a3828f5a51fe3a0bf71046ec58
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-07-27 13:43:18 +0900

    [level 2] Title: 타겟 넘버, Time: 392.88 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,28 @@
+def calculate_dfs(numbers, target, target_count, current_idx, calculated):
+    # print(current_idx, calculated)
+    
+    # 종료 조건: numbers의 모든 값에 대해 연산이 종료
+    # -> current_idx 값이 numbers인덱스 마지막 값을 넘게됨
+    if current_idx == len(numbers):
+        if target == calculated:
+            target_count += 1
+            return 1
+        else:
+            return 0
+    
+    # 경우의 수 2가지 -> 현재 상태에서 값을 더하는 경우와 빼는 경우
+    cal_add = calculate_dfs(numbers, target, target_count, current_idx+1, calculated+numbers[current_idx])
+    cal_sub = calculate_dfs(numbers, target, target_count, current_idx+1, calculated-numbers[current_idx])
+    
+    return cal_add + cal_sub
+
+
+def solution(numbers, target):
+    target_count = 0
+    
+    # 재귀호출 중단할 파라미터: current_idx
+    # 누적해서 결과를 가져갈 파라미터: calculated
+    # 조건을 충족하는 결과 개수를 저장할 파라미터: target_count
+    target_count += calculate_dfs(numbers, target, target_count, 0, 0)
+    
+    return target_count
