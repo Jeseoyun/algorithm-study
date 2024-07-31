3a616a131070bff7c19d87e496def6f0e50b6cca
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-07-31 12:11:56 +0900

    [D3] Title: 부분 수열의 합, Time: 3,968 ms, Memory: 81,032 KB -BaekjoonHub

---
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -0,0 +1,36 @@
+class Counter:
+    def __init__(self):
+        self.cnt = 0
+
+    def increase(self):
+        self.cnt += 1
+
+
+def subset_sum(arr, k, idx, subset, counter):
+    if idx == len(arr):
+        if sum(subset) == k:
+            counter.increase()
+        return
+
+    # 현재 원소를 포함 하거나
+    subset_sum(arr, k, idx+1, subset+[arr[idx]], counter)
+
+    # 현재 원소를 포함하지 않거나
+    subset_sum(arr, k, idx+1, subset, counter)
+
+
+def main():
+    T = int(input())
+
+    for test_case in range(1, T + 1):
+        N, K = map(int, input().split())
+        arr = list(map(int, input().split()))
+
+        result = Counter()
+        subset_sum(arr, K, 0, [], result)
+
+        print(f"#{test_case} {result.cnt}")
+
+
+if __name__ == "__main__":
+    main()
