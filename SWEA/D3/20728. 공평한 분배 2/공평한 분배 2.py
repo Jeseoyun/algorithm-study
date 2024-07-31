62857c5404dde2a6528ffa92ba280639fdbbaa29
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-07-31 13:52:35 +0900

    [D3] Title: 공평한 분배 2, Time: 128 ms, Memory: 48,180 KB -BaekjoonHub

--- SWEA/D3/20728. 공평한 분배 2/공평한 분배 2.py
+++ SWEA/D3/20728. 공평한 분배 2/공평한 분배 2.py
@@ -1,22 +1,20 @@
-def func(n, k, an):
-    sorted_an = sorted(an)
+def main():
+    T = int(input())
 
-    min_diff = 10**9
-    for i in range(n-k+1):
-        diff = sorted_an[i+k-1] - sorted_an[i]
-        if diff < min_diff:
-            min_diff = diff
+    for test_case in range(1, T + 1):
+        N, K = map(int, input().split())
+        candy_bundle = list(map(int, input().split()))
 
-    return min_diff
+        # 정렬하고
+        candy_bundle.sort()
+        # 연속된 놈들의 조합에서 최대, 최소 구한다
+        min_diff = float('inf')
+        for i in range(len(candy_bundle)-K+1):
+            candy = candy_bundle[i:i+K]
+            min_diff = min(min_diff, candy[-1]-candy[0])
+
+        print(f"#{test_case} {min_diff}")
 
 
 if __name__ == "__main__":
-    T = int(input())
-
-    for test_case in range(T):
-        n, k = map(int, input().split())  # 주머니 개수, 나눠 줄 주머니 개수
-        an = list(map(int, input().split()))
-
-        result = func(n, k, an)
-
-        print(f"#{test_case+1} {result}")
+    main()
