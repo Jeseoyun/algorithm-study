b76f00bdb83f38dfcb4de090179095c21567d201
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-08-05 22:25:48 +0900

    Update 설탕 배달.py

--- PyPy3/백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ PyPy3/백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,3 +1,28 @@
+def get_min_comb(N, max_m, max_n):
+    min_comb = float('inf')
+    
+    for m in range(max_m): # 큰 숫자를 먼저 박기
+        for n in range(max_n):
+            if 5*m + 3*n == N:
+                min_comb = min(min_comb, m + n)
+            elif 5*m + 3*n > N:
+                break
+            
+    return min_comb if min_comb != float('inf') else -1
+
+
+# fail: 메모리 초과
+def get_min_comb_dfs(N, m, n, comb):
+    if 5*m + 3*n == N:
+        comb.append(m + n)
+        return 
+    if 5*m + 3*n > N:
+        return
+    
+    get_min_comb_dfs(N, m+1, n, comb)
+    get_min_comb_dfs(N, m, n+1, comb)
+
+
 def main():
     N = int(input())
     
@@ -5,19 +30,12 @@ def main():
     max_m = N//5 + 1
     max_n = N//3 + 1
     
-    min_comb = float('inf')
+    result = get_min_comb(N, max_m, max_n)
+    # comb = []
+    # get_min_comb_dfs(N, 0, 0, comb)
+    # result = min(comb) if comb else -1
     
-    for m in range(max_m):
-        for n in range(max_n):
-            if 5*m + 3*n == N:
-                min_comb = min(min_comb, m + n)
-            elif 5*m + 3*n > N:
-                break
-            
-    if min_comb == float('inf'):
-        min_comb = -1
-            
-    print(min_comb)
+    print(result)
 
 
 if __name__ == "__main__":
