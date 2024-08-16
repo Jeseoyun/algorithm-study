f1846ada5e41414eb2a257579ac95d8eb8591a1e
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-08-16 11:04:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 6,105 ms, Memory: 85,120 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.py
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.py
@@ -1,3 +1,27 @@
+def make_sparse_matrix(row, col, arr_2d):
+    sparse_matrix = dict()
+    for i in range(row):
+        for j in range(col):
+            if arr_2d[i][j] == 0:  continue
+            key = (i, j)  # key: 좌표값
+            value = [arr_2d[i][j], arr_2d[i][j], False]  # value: [생명력 수치, 생명력 카운트(초기값은 생명력 수치와 같다), 활성 상태 체크]
+            sparse_matrix[key] = value
+
+    return sparse_matrix
+
+
+def calculate_alive_cells(cells: dict):
+    alive_cnt = 0
+    for cell_info in cells.values():
+        _, vital_cnt, active = cell_info
+        
+        if active and vital_cnt == 0:  # 죽은 세포는 계산 안한다
+            continue
+
+        alive_cnt += 1
+    return alive_cnt
+
+
 def main():
     T = int(input())
     
@@ -5,35 +29,38 @@ def main():
         N, M, K = map(int, input().split())  # 세로 크기, 가로 크기, 배양 시간
         grid = [list(map(int, input().split())) for _ in range(N)]
 
-        cells = dict()
-        for i in range(N):
-            for j in range(M):
-                if grid[i][j] == 0:  continue
-                # (x좌표, y좌표): [[생명력 수치, 생명력 카운트, 활성 상태 체크], [생명력 수치, 활성 상태 체크]]
-                cells[(i, j)] = [grid[i][j], grid[i][j], False]
-        time = 0
+        '''
+        grid의 대부분의 값이 0으로 채워져 있음
+        그리고 grid의 크기는 문제에서 무한하다 하였으므로 dictionary 기반의 sparse matrix 구조 사용
+
+        sparse matrix 장점
+        - 배열에서 값이 존재하는 위치만을 저장할 수 있으므로 대부분의 값이 0인 경우 메모리 절약 가능
+        - 무한히 큰 배열에서도 필요한 부분만 다룰 수 있음
+
+        sparse matrix 단점
+        - 모든 셀에 값을 채우는 경우 리스트보다 느릴 수 있다
+        '''
+        cells = make_sparse_matrix(N, M, grid)
+
+        # K 시간 동안 줄기세포 배양 진행
         while K:
-            # print(f"=============== {time}시간 후, {cells} ===============")
             spread = {}  # 새로운 세포 번식을 기록할 딕셔너리
-            for pos in list(cells.keys()):
+            for pos in cells.keys():
                 # print(pos, cells[pos])
                 x, y = pos
                 vitality, vital_cnt, active = cells[pos]
 
                 # 1. 비활성일 경우
                 if not active:
-                    if vital_cnt > 0:
+                    if vital_cnt > 0:  # 생명력 수치가 남아있을 경우 -> 생명력 수치 1 감소
                         cells[pos][1] -= 1
-                        # print(f"비활성 | 생명력 감소 | 원래 생명력: {vitality} | 남은 생명력: {cells[pos][0]}")
-                    if cells[pos][1] == 0:
-                        cells[pos] = [vitality, vitality, True]  # 생명력 최대로 초기화
-                        # print(f"비활성 | 활성 상태 전환 | 바뀐 값: {cells[pos]}")
+                    if cells[pos][1] == 0:  # 활성 상태에 진입 -> 생명력 최대로 초기화
+                        cells[pos] = [vitality, vitality, True]
 
                 # 2. 활성일 경우
                 else:
-                    if vital_cnt == 0:
-                        # print(f"활성 | 죽은 셀")
-                        continue
+                    # 활성 상태 이면서 생명력 수치가 0일 경우 죽은 세포임
+                    if vital_cnt == 0:  continue
                     
                     # 상하좌우로 세포 번식
                     dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -41,36 +68,28 @@ def main():
                         nx, ny = x + dx, y + dy
                         new_pos = (nx, ny)
 
-                        if new_pos not in cells:
-                            if new_pos not in spread:
+                        if new_pos not in cells.keys():  # dict.keys()도 hash로 동작해서 평균 시간 복잡도 O(1)이라고 한다
+                            if new_pos not in spread.keys():
                                 spread[new_pos] = [vitality, vitality, False]
                             else:
-                                # 생명력 비교하여 더 큰 생명력 가진 세포가 우선한다
+                                # 생명력 비교하여 더 큰 생명력 가진 세포가 먹는다
                                 if spread[new_pos][0] < vitality:
                                     spread[new_pos] = [vitality, vitality, False]
-                            # print(f"활성 | 번식 | ({dx}, {dy}) 방향 | 번식 좌표: {new_pos} | {spread[new_pos]}")
 
                     cells[pos][1] -= 1
-                    # print(f"활성 | 생명력 감소 | 원래 생명력: {vitality} | 남은 생명력: {cells[pos][0]}")
 
             # spread 딕셔너리에 기록된 새로운 세포를 cells에 추가
+            # if not spread:  continue
             for new_pos, cell_info in spread.items():
                 if new_pos in cells:
                     if cells[new_pos][0] < cell_info[0]:
                         cells[new_pos].append(cell_info)
                 else:
                     cells[new_pos] = cell_info
-            # print("바뀐 셀: ", cells)
+            
             K -= 1  # 시간 감소
-            time += 1
         
-        result = 0
-        for pos, cell_info in cells.items():
-            vitality, vital_cnt, active = cell_info
-            # 활성 상태인 세포 개수 계산 (죽은 세포는 계산 안한다)
-            if active and vital_cnt == 0:
-                continue
-            result += 1
+        result = calculate_alive_cells(cells)
 
         print(f"#{test_case} {result}")
 
