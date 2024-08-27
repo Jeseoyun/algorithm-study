f0c5c9774ab36132e6b6b413519d4e5f89701f9a
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-08-28 00:02:37 +0900

    [level 3] Title: 등굣길, Time: 3.13 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,19 @@
+def solution(m, n, puddles):
+    puddles = set(tuple(p) for p in puddles)
+    if (1, 1) in puddles or (m, n) in puddles:
+        return 0
+    
+    path = [[0]*m for _ in range(n)]
+    path[0][0] = 1
+    
+    for i in range(n):
+        for j in range(m):
+            if (j+1, i+1) in puddles:
+                path[i][j] = 0
+            else:
+                if i > 0:
+                    path[i][j] += path[i-1][j]
+                if j > 0:
+                    path[i][j] += path[i][j-1]
+                
+    return path[n-1][m-1] % 1000000007
