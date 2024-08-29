42ad4c760a3e446e2e9edf0a5b25a2a21d8af4ab
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-08-29 15:26:27 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 475 ms, Memory: 69,184 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
@@ -1,87 +1,85 @@
-from collections import deque
-import copy
+dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+max_length = 0
 
 
-def find_start_point(map, N):
-    peak = 0
-    peak_points = []
-    
-    for i in range(N):
-        for j in range(N):
-            if peak < map[i][j]:
-                peak = map[i][j]
-                peak_points = [(i, j)]
-            elif peak == map[i][j]:
-                peak_points += [(i, j)]
-            
-    return peak_points
+def find_highest_point(map, map_size):
+    max_height = 0
+    hp = []
+    for i in range(map_size):
+        for j in range(map_size):
+            if max_height < map[i][j]:
+                max_height = map[i][j]
+                hp = []
+            if max_height == map[i][j]:
+                hp.append((i, j))
+    return hp
 
 
-def hiking_trail_bfs(hiking_map, N, K, start_x, start_y):
-    dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+def dfs(hiking_map, map_size, x, y, visited, path, K, FLAG=False):
+    global max_length
 
-    # 시작 지점 초기값 설정
-    path_init = {(start_x, start_y)}
-    queue = deque([(start_x, start_y, path_init, hiking_map[start_x][start_y], False)])
-    # queue => (x좌표, y좌표, 지금까지 방문 경로, 현재 지점의 높이, 깎은 적이 있는지, 이동한 길이)
-
-    max_length = 1
+    for dx, dy in dxy:
+        nx, ny = x + dx, y + dy
         
-    while queue:
-        # print(queue)
-        x, y, path, curr_height, FLAG = queue.popleft()
-        
-        for dx, dy in dxy:
-            nx, ny = x + dx, y + dy
+        # 배열 범위 벗어나는 경우
+        if nx < 0 or nx >= map_size or ny < 0 or ny >= map_size:
+            continue
 
-            # 범위 벗어난 경우
-            if nx < 0 or nx >= N or ny < 0 or ny >= N:
-                continue
-            
-            # 이미 방문한 경우
-            if (nx, ny) in path:
-                continue
-            
-            # 1. 이동할 지점이 더 낮은 경우 -> 그 지점으로 바로 이동
-            if hiking_map[nx][ny] < curr_height:
-                new_path = copy.deepcopy(path)  # 집합은 리스트처럼 mutable -> deepcopy 해줘야함
-                new_path.add((nx, ny))
-                queue.append((nx, ny, new_path, hiking_map[nx][ny], FLAG))
-            
-            # 2. 이동할 지점이 더 높거나 같은 경우(elif 조건문 사용)
-            # K만큼 깎을 수 잇는 기회 1번 있음 
-            # K를 썼는지 안썼는지 알기 위해 FLAG를 두자
-            # -> 이전에 K만큼 지형 깎은 적 없으면 깎고 이동
-            elif not FLAG:
-                if hiking_map[nx][ny] - curr_height + 1 <= K:
-                    new_path = copy.deepcopy(path)
-                    new_path.add((nx, ny))
-                    # hiking_map[nx][ny] = hiking_map[x][y] - 1   # 일케 하면 배열 자체가 아예 바뀌어버림
-                    new_height = curr_height - 1  # 현재 지점보다 1 작으면 최소로 깎는거시다
-                    queue.append((nx, ny, new_path, new_height, True))
+        # 이미 방문한 곳일 경우
+        if visited[nx][ny]:
+            continue
 
-            max_length = max(max_length, len(new_path))
+        # 다음 지점의 높이가 현재 지점의 높이보다 낮으면 방문 가능
+        if hiking_map[nx][ny] < path[-1]:
+            visited[nx][ny] = 1
+            path.append(hiking_map[nx][ny])
 
-    return max_length
+            max_length = max(max_length, len(path))
+
+            dfs(hiking_map, map_size, nx, ny, visited, path, K, FLAG)
+
+            visited[nx][ny] = 0
+            path.pop(-1)
+
+        # 단 1회, 지형을 깎을 수 있다
+        elif not FLAG and hiking_map[nx][ny] - K < path[-1]: 
+            FLAG = True
+
+            visited[nx][ny] = 1
+            path.append(path[-1] - 1)  # 이전 지점보다 딱 1 낮게 깎으면 최소로 깎는 거시다
+
+            max_length = max(max_length, len(path))
+
+            dfs(hiking_map, map_size, nx, ny, visited, path, K, FLAG)
+
+            FLAG = False
+            visited[nx][ny] = 0
+            path.pop(-1)
+
 
 
 def main():
+    global max_length
+
     T = int(input())
-    
+
     for test_case in range(1, T+1):
         N, K = map(int, input().split())
         hiking_map = [list(map(int, input().split())) for _ in range(N)]
-        
-        # 1. 가장 높은 지점 찾기
-        start_points = find_start_point(hiking_map, N)
-        
-        # 2. bfs 순회
-        root_length = []
-        for start_x, start_y in start_points:
-            root_length.append(hiking_trail_bfs(hiking_map, N, K, start_x, start_y))
-        
-        result = max(root_length)
-        print(f"#{test_case} {result}")
+        highest_points = find_highest_point(hiking_map, N)
+
+        max_length = 0
+        for start_point in highest_points:
+            sx, sy = start_point
+            visited = [[0] * N for _ in range(N)]
+            path = []
+
+            visited[sx][sy] = 1
+            path.append(hiking_map[sx][sy])
+
+            dfs(hiking_map, N, sx, sy, visited, path, K)
+            
+        print(f"#{test_case} {max_length}")
 
 
 if __name__ == "__main__":
