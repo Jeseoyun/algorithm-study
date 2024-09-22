b6e0e0c08e91c6a8b12e3854a214c48f780b7b35
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2024-09-22 23:41:02 +0900

    [Silver II] Title: DFS와 BFS, Time: 192 ms, Memory: 118624 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+
+def dfs(curr, visited):
+    visited.append(curr)
+
+    if len(visited) == N:
+        return list(map(str, visited))
+
+    for adj in adj_list[curr]:
+        if adj not in visited:
+            dfs(adj, visited)
+
+    return list(map(str, visited))
+
+def bfs():
+    visited = []
+    queue = deque()
+
+    queue.append(V)
+    visited.append(V)
+
+    while queue:
+        curr = queue.popleft()
+        for adj in adj_list[curr]:
+            if adj in visited:
+                continue
+
+            queue.append(adj)
+            visited.append(adj)
+    return list(map(str, visited))
+
+
+N, M, V = map(int, input().split())  # vertex 수, edge 수, 탐색 시작 정점
+edges = [list(map(int, input().split())) for _ in range(M)]
+
+adj_list = {v: [] for v in range(1, N+1)}
+
+for edge in edges:
+    adj_list[edge[0]].append(edge[1])
+    adj_list[edge[1]].append(edge[0])
+
+adj_list = {adj: sorted(adj_list[adj]) for adj in adj_list}
+
+print(" ".join(dfs(V, [])))
+print(" ".join(bfs()))
