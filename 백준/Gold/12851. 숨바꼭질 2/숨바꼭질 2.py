425ae9953c47ad906070ad95eff82629eb6bfdf4
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2025-07-14 22:51:29 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 216 ms, Memory: 129432 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+LIMIT = 100001
+INF = float('inf')
+
+def main():
+    n, k = map(int, input().split())  # 시작점, 도착점
+
+    if n == k:
+        print(0, 1, sep="\n")
+        return
+
+    queue = deque([n])
+    visited = [INF for _ in range(LIMIT)]
+    visited[n] = 0
+
+    min_time = INF
+    min_time_cnt = 0
+
+    while queue:
+        x = queue.popleft()
+
+        for nx in (x - 1, x + 1, 2 * x):
+            if nx < 0 or nx >= LIMIT:
+                continue
+            if visited[nx] < visited[x] + 1:
+                continue
+
+            if nx == k:
+                if min_time > visited[x] + 1:
+                    min_time = visited[x] + 1
+                    min_time_cnt = 1
+                elif min_time == visited[x] + 1:
+                    min_time_cnt += 1
+
+            if visited[nx] >= visited[x] + 1:
+                visited[nx] = visited[x] + 1
+                queue.append(nx)
+
+    print(min_time)
+    print(min_time_cnt)
+
+if __name__ == "__main__":
+    main()
