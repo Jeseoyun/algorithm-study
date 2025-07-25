ae30180e7be56cb068abe39cb98fcaef90c904c3
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2025-07-25 16:22:22 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 119660 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,15 @@
-def dfs(N, M, i, curr, visited, result):
-    if i == M:
+def dfs(n, m, step, curr, visited, result):
+    if step == m:
         result.append(curr)
         return
 
-    for j in range(1, N+1):
-        if visited[j] == 1:
+    for i in range(1, n+1):
+        if visited[i] == 1:
             continue
-        
-        visited[j] = 1
-        dfs(N, M, i+1, curr+[j], visited, result)
-        visited[j] = 0
+
+        visited[i] = 1
+        dfs(n, m, step+1, curr+[i], visited, result)
+        visited[i] = 0
 
 
 def main():
@@ -17,12 +17,10 @@ def main():
 
     result = []
     visited = [0]*(N+1)
-
     dfs(N, M, 0, [], visited, result)
 
     for res in result:
         print(*res)
-    # print(result)
 
 
 if __name__ == "__main__":
