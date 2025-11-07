77d2007b5488579ea221e602e1a3f7754c007b1c
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2025-11-07 21:38:42 +0900

    [Gold IV] Title: 알파벳, Time: 6716 ms, Memory: 160120 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -2,28 +2,32 @@ dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상, 하, 좌, 우
 max_len = 0
 
 
-def dfs(board, R, C, x, y, visited, history):
+def dfs(board, R, C, x, y, alphabet, history):
     global max_len
     # print("x, y:", (x, y), board[x][y])
     max_len = max(max_len, len(history))
 
     for dx, dy in dxy:
         nx, ny = x + dx, y + dy
+
         if nx < 0 or nx >= R or ny < 0 or ny >= C:
             continue
 
-        if visited[nx][ny]:
+        alp_idx = ord(board[nx][ny])- ord('A')
+
+        if alphabet[alp_idx]:
             continue
 
         if board[nx][ny] in history:
             continue
 
-        visited[nx][ny] = True
+
+        alphabet[alp_idx] = True
         history.add(board[nx][ny])
 
-        dfs(board, R, C, nx, ny, visited, history)
+        dfs(board, R, C, nx, ny, alphabet, history)
 
-        visited[nx][ny] = False
+        alphabet[alp_idx] = False
         history.remove(board[nx][ny])
 
 
@@ -31,10 +35,9 @@ def main():
     R, C = map(int, input().split())
     board = [list(input()) for _ in range(R)]
 
-    visited = [[False]*C for _ in range(R)]
-    visited[0][0] = True
+    alphabet = [False]*26  # 26개
     history = set(board[0][0])
-    dfs(board, R, C, 0, 0, visited, history)
+    dfs(board, R, C, 0, 0, alphabet, history)
 
     print(max_len)
 
