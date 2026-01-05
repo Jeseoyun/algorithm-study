4f93340f24b3e4094825c75e32de32d0db23b576
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2026-01-06 01:04:08 +0900

    [Silver V] Title: 폴리오미노, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -0,0 +1,14 @@
+def main():
+    board = input().strip()
+
+    board = board.replace("XXXX", "AAAA")
+    board = board.replace("XX", "BB")
+
+    if "X" in board:
+        print(-1)
+    else:
+        print(board)
+
+
+if __name__ == "__main__":
+    main()
