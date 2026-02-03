2ea051e2dfe36fc1c8187f89134496efdc43eb95
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2026-02-03 12:10:32 +0900

    [Silver III] Title: 가희와 키워드, Time: 956 ms, Memory: 148304 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.py
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.py
@@ -2,10 +2,13 @@ def main():
     N, M = map(int, input().split())
     keywords = set(input() for _ in range(N))
 
+    out = []
     for _ in range(M):
         memos = set(input().split(","))
         keywords -= memos
-        print(len(keywords))
+        out.append(len(keywords))
+
+    print(*out, sep="\n")
 
 
 if __name__ == "__main__":
