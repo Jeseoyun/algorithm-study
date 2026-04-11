c6a9e281497b307b26865db91e59647fc6030c19
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2026-04-11 23:56:29 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260411/계단 오르기/climbing-stairs.py
@@ -0,0 +1,17 @@
+def main():
+    N = int(input())
+    step = [0]*(N+1)
+
+    step[2] = 1
+    
+    if N >= 3:
+        step[3] = 1
+    
+    for i in range(4, N+1):
+        step[i] = step[i-2] + step[i-3]
+    
+    print(step[N])
+
+
+if __name__ == "__main__":
+    main()
